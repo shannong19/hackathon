c7ff65d8a3611b1196b8d848874b0d208a3dfcdf
Shannon <sgallagh@andrew.cmu.edu>
2016-02-15 11:33:02 -0500

    Set up initial files

---
+++ server.R
@@ -0,0 +1,24 @@
+library(shiny)
+library(leaflet)
+library(RColorBrewer)
+library(scales)
+library(lattice)
+library(dplyr)
+
+
+
+shinyServer(function(input, output, session) {
+
+  ## Interactive Map ###########################################
+
+  # Create the map
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles(
+        urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+      ) %>%
+      setView(lng = -93.85, lat = 37.45, zoom = 4)
+  })
+
+})
