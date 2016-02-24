ee3bdbbab25b536795b318d716118813e0db82ad
Shannon <sgallagh@stat.cmu.edu>
2016-02-24 18:49:43 -0500

    Changed layout

--- server.R
+++ server.R
@@ -1,18 +1,19 @@
-library(shiny)
-library(leaflet)
-library(RColorBrewer)
-library(scales)
-library(lattice)
-library(dplyr)
+#server
 
 
+function(input, output) {
 
-shinyServer(function(input, output, session) {
+set.seed(122)
+  histdata <- rnorm(500)
 
-  ## Interactive Map ###########################################
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
 
-  # Create the map
-  output$map <- renderLeaflet({
+
+    #map
+    output$map <- renderLeaflet({
     leaflet() %>%
       addTiles(
         urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
@@ -21,4 +22,60 @@ shinyServer(function(input, output, session) {
       setView(lng = -93.85, lat = 37.45, zoom = 4)
   })
 
-})
+
+
+    
+## # 1=South, 2=East, 3=West, 4=North
+## dirColors <-c("1"="#595490", "2"="#527525", "3"="#A93F35", "4"="#BA48AA")
+
+
+        
+##   # Store last zoom button value so we can detect when it's clicked
+##   lastZoomButtonValue <- NULL
+
+##   output$busmap <- renderLeaflet({
+##     locations <- NULL
+##     if (length(locations) == 0)
+##       return(NULL)
+
+##     # Show only selected directions
+##     locations <- filter(locations, Direction %in% as.numeric(input$directions))
+
+##     # Four possible directions for bus routes
+##     dirPal <- colorFactor(dirColors, names(dirColors))
+
+##     map <- leaflet(locations) %>%
+##       addTiles('http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png') %>%
+##       addCircleMarkers(
+##         ~VehicleLongitude,
+##         ~VehicleLatitude,
+##         color = ~dirPal(Direction),
+##         opacity = 0.8,
+##         radius = 8
+##       )
+
+##     if (as.numeric(input$routeNum) != 0) {
+##       route_shape <- get_route_shape(input$routeNum)
+
+##       map <- addPolylines(map,
+##         route_shape$shape_pt_lon,
+##         route_shape$shape_pt_lat,
+##         fill = FALSE
+##       )
+##     }
+
+##     rezoom <- "first"
+##     # If zoom button was clicked this time, and store the value, and rezoom
+##     if (!identical(lastZoomButtonValue, input$zoomButton)) {
+##       lastZoomButtonValue <<- input$zoomButton
+##       rezoom <- "always"
+##     }
+
+##     map <- map %>% mapOptions(zoomToLimits = rezoom)
+
+##     map
+##   })
+
+    
+    
+}
--- ui.R
+++ ui.R
@@ -1,5 +1,8 @@
 library(shiny)
+library(shinydashboard)
 library(leaflet)
+library(dplyr)
+#library(curl) 
 
 # Choices for drop-downs
 vars <- c(
@@ -11,79 +14,127 @@ vars <- c(
 )
 
 
-shinyUI(navbarPage("SPEW View", id="nav",
 
-  tabPanel("Menu",
-           h1("Welcome to SPEW View!", style="color: #ad1d28")
-           ),
+dashboardPage(skin="yellow",
+  dashboardHeader(title = "SPEW VIEW"),
 
-  tabPanel("Modelling",
-           h1("Let's fit a model.")
-           ),
-                   
-  tabPanel("Interactive map",
-    div(class="outer",
-
-      tags$head(
-        # Include our custom CSS
-        includeCSS("styles.css"),
-        includeScript("gomap.js")
-      ),
-
-      leafletOutput("map", width="100%", height="100%"),
-
-      # Shiny versions prior to 0.11 should use class="modal" instead.
-      absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-        draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-        width = 330, height = "auto",
-
-        h2("ZIP explorer"),
-
-        selectInput("color", "Color", vars),
-        selectInput("size", "Size", vars, selected = "adultpop"),
-        conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
-          # Only prompt for threshold when coloring or sizing by superzip
-          numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
-        ),
-
-        plotOutput("histCentile", height = 200),
-        plotOutput("scatterCollegeIncome", height = 250)
-      ),
-
-      tags$div(id="cite",
-        'Data compiled for ', tags$em('Coming Apart: The State of White America, 1960–2010'), ' by Charles Murray (Crown Forum, 2012).'
-      )
+ ## Sidebar content
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Widgets", tabName = "widgets", icon = icon("th")),
+      menuItem("Map", tabName="map", icon=icon("map"))
     )
   ),
 
-  tabPanel("Data explorer",
-    fluidRow(
-      column(3,
-        selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE)
-      ),
-      column(3,
-        conditionalPanel("input.states",
-          selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
+  ## Body content
+  dashboardBody(
+      tags$head(
+          tags$link(rel = "stylesheet", type="text/css", href = "custom.css")
+          ),
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+        fluidRow(
+          box(plotOutput("plot1", height = 250)),
+
+          box(
+            title = "Controls",
+            sliderInput("slider", "Number of observations:", 1, 100, 50)
+          )
         )
       ),
-      column(3,
-        conditionalPanel("input.states",
-          selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
-        )
-      )
-    ),
-    fluidRow(
-      column(1,
-        numericInput("minScore", "Min score", min=0, max=100, value=0)
+
+      # Second tab content
+      tabItem(tabName = "widgets",
+              tags$head(
+          tags$link(rel = "stylesheet", type="text/css", href = "custom.css")
+          ),
+        h2("Widgets tab content")
       ),
-      column(1,
-        numericInput("maxScore", "Max score", min=0, max=100, value=100)
-      )
-    ),
-    hr(),
-    DT::dataTableOutput("ziptable")
-  ),
 
-  conditionalPanel("false", icon("crosshair"))
-))
+      #third item
+      tabItem(tabName = "map",
+              fluidRow(
+                  column(width = 9,
+                         box(width = NULL, solidHeader = TRUE,
+                             leafletOutput("map", height = 500)
+                             ),
+                         box(width = NULL,
+                             uiOutput("numVehiclesTable")
+                             )
+                         ),
+                  column(width = 3,
+                         box(width = NULL, status = "warning",
+                             uiOutput("routeSelect"),
+                             checkboxGroupInput("directions", "Show",
+                                                choices = c(
+                                                    Northbound = 4,
+                                                    Southbound = 1,
+                                                    Eastbound = 2,
+                                                    Westbound = 3
+                                                ),
+                                                selected = c(1, 2, 3, 4)
+                                                ),
+                             p(
+                                 class = "text-muted",
+                                 paste("Note: a route number can have several different trips, each",
+                                       "with a different path. Only the most commonly-used path will",
+                                       "be displayed on the map."
+                                       )
+                             ),
+                             actionButton("zoomButton", "Zoom to fit buses")
+                             ),
+                         box(width = NULL, status = "warning",
+                             selectInput("interval", "Refresh interval",
+                                         choices = c(
+                                             "30 seconds" = 30,
+                                             "1 minute" = 60,
+                                             "2 minutes" = 120,
+                                             "5 minutes" = 300,
+                                             "10 minutes" = 600
+                                         ),
+                                         selected = "60"
+                                         ),
+                             uiOutput("timeSinceLastUpdate"),
+                             actionButton("refresh", "Refresh now"),
+                             p(class = "text-muted",
+                               br(),
+                               "Source data updates every 30 seconds."
+                               )
+                             
+                             )
+                         )
+                 
+              )
+              )
+    )
+  )
 
+  )
+   
+
+
+      ## # Shiny versions prior to 0.11 should use class="modal" instead.
+      ## absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+      ##   draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+      ##   width = 330, height = "auto",
+
+      ##   h2("ZIP explorer"),
+
+      ##   selectInput("color", "Color", vars),
+      ##   selectInput("size", "Size", vars, selected = "adultpop"),
+      ##   conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+      ##     # Only prompt for threshold when coloring or sizing by superzip
+      ##     numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+      ##   ),
+
+      ##   plotOutput("histCentile", height = 200),
+      ##   plotOutput("scatterCollegeIncome", height = 250)
+      ## )## ,
+
+      ## tags$div(id="cite",
+      ##   'Data compiled for ', tags$em('Coming Apart: The State of White America, 1960–2010'), ' by Charles Murray (Crown Forum, 2012).'
+      ##   ) 
+
+      #####################################33
